1ff59849286f7bb9ea3207926f76e025b5a3cb32
islam <139281411+islam-sniper@users.noreply.github.com>
2023-08-02 23:32:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('button');
+let inp = document.getElementById('input');
+    function gan(){
+        btn.innerHTML= 'امسح احبيبى و اكتب عدل';
+        setTimeout(function(){inp.value=''},5000);
+        setTimeout(function(){btn.innerHTML='send'},5000);
+    }
+
