81760fc3c19cb9348df1bcddd0002c8975f868f4
islam <139281411+islam-sniper@users.noreply.github.com>
2023-08-03 13:45:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let btn = document.getElementById('button');
 let inp = document.getElementById('input');
+let inp1 = document.getElementById('input1');
+let inp2 = document.getElementById('input2');
+let inp3 = document.getElementById('input3');
+let inp4 = document.getElementById('input4');
     function gan(){
         btn.innerHTML= 'امسح احبيبى و اكتب عدل';
-        setTimeout(function(){inp.value=''},5000);
-        setTimeout(function(){btn.innerHTML='send'},5000);
+        setTimeout(function(){inp.value=''},7000);
+        setTimeout(function(){inp1.value=''},7000);
+        setTimeout(function(){inp2.value=''},7000);
+        setTimeout(function(){inp3.value=''},7000);
+        setTimeout(function(){inp4.value=''},7000);
+        setTimeout(function(){btn.innerHTML='send'},7000);
     }
 
